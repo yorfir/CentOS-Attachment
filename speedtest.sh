00390fdc826f9f40219f924fa55ba484c30b4387
Yorfir <yorfir@users.noreply.github.com>
2017-04-28 10:21:22 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,4 +1,4 @@
-wget https://raw.github.com/sivel/speedtest-cli/master/speedtest.py
+wget --no-check-certificate https://raw.github.com/sivel/speedtest-cli/master/speedtest.py
 chmod a+rx speedtest.py
 mv speedtest.py /usr/local/bin/speedtest
 chown root:root /usr/local/bin/speedtest
