a9b28f12f24945a3b621d12a18a9ebf6bf7fb611
Yorfir <yorfir@users.noreply.github.com>
2017-04-29 23:05:06 +0800

    Update auto-install-nload.sh

--- auto-install-nload.sh
+++ auto-install-nload.sh
@@ -4,3 +4,4 @@ wget http://www.roland-riegel.de/nload/nload-0.7.4.tar.gz
 tar zxvf nload-0.7.4.tar.gz
 cd nload-0.7.4
 ./configure;make;make install
+echo -e "\033[37;31;5mnload安装成功，输入nload查看...\033[39;49;0m"
