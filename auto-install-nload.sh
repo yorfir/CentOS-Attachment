3ff032e873bf5ca21615765e9f7583610a1b6f0f
Yorfir <yorfir@users.noreply.github.com>
2017-04-29 22:45:03 +0800

    Update auto-install-nload.sh

--- auto-install-nload.sh
+++ auto-install-nload.sh
@@ -1,5 +1,5 @@
-yum install gcc gcc-c++ kernel-devel
-yum install ncurses-devel
+yum -y install gcc gcc-c++ kernel-devel
+yum -y install ncurses-devel
 wget http://www.roland-riegel.de/nload/nload-0.7.4.tar.gz
 tar zxvf nload-0.7.4.tar.gz
 cd nload-0.7.4
