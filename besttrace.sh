ea8ba7b9706ecb96b9a6cd65fe4a0bc588dcd9c9
Yorfir <yorfir@users.noreply.github.com>
2017-05-03 21:50:25 +0800

    Update besttrace.sh

--- besttrace.sh
+++ besttrace.sh
@@ -4,4 +4,4 @@ wget --no-check-certificate https://raw.githubusercontent.com/yorfir/CentOS-Atta
 yum -y install unzip
 unzip besttrace4linux.zip
 chmod a+x besttrace
-echo -e "\033[37;31;5mbesttrace安装成功，输入.\/besttrace ip 使用...\033[39;49;0m"
+echo -e "\033[37;31;5mbesttrace安装成功，输入./besttrace+ip 使用...\033[39;49;0m"
