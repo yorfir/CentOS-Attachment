673a94aa2746a5ecc69808527a784064e77ce17e
Yorfir <yorfir@users.noreply.github.com>
2017-05-31 01:46:53 +0800

    Update install-snmp.sh

--- install-snmp.sh
+++ install-snmp.sh
@@ -1,1 +1,1 @@
-yum install -y net-snmp && rocommunity zuanshi 128.1.235.43 && service snmpd start && chkconfig snmpd on && iptables -A INPUT  -p udp -s 128.1.235.43 --dport 161 -j ACCEPT && service iptables save
+yum install -y net-snmp && echo "rocommunity zuanshi 128.1.235.43">>/etc/snmp/snmpd.conf && service snmpd start && chkconfig snmpd on && iptables -A INPUT  -p udp -s 128.1.235.43 --dport 161 -j ACCEPT && service iptables save
