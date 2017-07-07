4a221611cc259d1ef10cde027f13f65249008528
Yorfir <yorfir@users.noreply.github.com>
2017-07-07 12:51:28 +0800

    Create install-lotserver-appex.sh

---
+++ install-lotserver-appex.sh
@@ -0,0 +1,5 @@
+cd /root
+yum install wget -y && wget http://download.appexnetworks.com.cn/d/ls/lotServerInstaller.tar.gz
+tar xzvf lotServerInstaller.tar.gz
+chmod +X lotServerInstaller.sh
+bash lotServerInstaller.sh
