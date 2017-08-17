01d5b1b994dcc96f59fa7b602debbffbc057b773
Yorfir <yorfir@users.noreply.github.com>
2017-08-17 17:17:02 +0800

    Update install-iftop.sh

--- install-iftop.sh
+++ install-iftop.sh
@@ -1,3 +1,11 @@
+yum -y install gcc-c++ && yum -y install flex && yum -y install bison
+cd /root
+wget http://www.tcpdump.org/release/libpcap-1.8.1.tar.gz
+tar zxvf libpcap-1.8.1.tar.gz
+cd libpcap-1.8.1
+./configure
+make && make install
+cd /root
 wget http://www.ex-parrot.com/~pdw/iftop/download/iftop-0.17.tar.gz
 tar zxvf iftop-0.17.tar.gz
 cd iftop-0.17
