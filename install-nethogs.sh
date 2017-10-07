5a806834f3ca91c327ff0f07dce212c52d5890b5
Yorfir <yorfir@users.noreply.github.com>
2017-10-07 08:56:33 -0700

    Update install-nethogs.sh

--- install-nethogs.sh
+++ install-nethogs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /root
-wget --no-check-certificate https://sourceforge.net/projects/nethogs/files/nethogs/0.8/nethogs-0.8.0.tar.gz/download
+wget --no-check-certificate https://downloads.sourceforge.net/project/nethogs/nethogs/0.8/nethogs-0.8.0.tar.gz
 yum -y install libpcap-devel  ncurses* gcc "gcc-c++.x86_64"
 tar zxvf nethogs-0.8.0.tar.gz
 cd nethogs-0.8.0
