e8040bbec07c27ff200469f6506a8715b90a47f3
Yorfir <yorfir@users.noreply.github.com>
2017-12-27 09:59:24 -0500

    Update install-rclone.sh

--- rclone/install-rclone.sh
+++ rclone/install-rclone.sh
@@ -6,4 +6,3 @@ sudo cp rclone /usr/bin/
 sudo chown root:root /usr/bin/rclone
 sudo chmod 755 /usr/bin/rclone
 yum install -y screen && screen -S rclone 
-curl https://raw.githubusercontent.com/yorfir/CentOS-Attachment/master/rclone/config/rclone.conf  -o /root/.config/rclone/rclone.conf --progress
