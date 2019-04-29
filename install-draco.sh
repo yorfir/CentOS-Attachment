3ab7b3b240d37b3b5c79932a24730bfaf2605a29
wuyukang <wuyukang@gmail.com>
2019-04-29 23:59:59 +0800

    no message

---
+++ install-draco.sh
@@ -0,0 +1,30 @@
+	if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        DISTRO='CentOS'
+        PM='yum'
+    elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+        DISTRO='RHEL'
+        PM='yum'
+    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+        DISTRO='Aliyun'
+        PM='yum'
+    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+        DISTRO='Fedora'
+        PM='yum'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        DISTRO='Debian'
+        PM='apt-get'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        DISTRO='Ubuntu'
+        PM='apt-get'
+    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        DISTRO='Raspbian'
+        PM='apt-get'
+    else
+        DISTRO='unknow'
+    fi
+	$PM install -y wget unzip 
+	cd /root
+	wget --no-check-certificate https://raw.githubusercontent.com/yorfir/CentOS-Attachment/master/source/draco-node.zip
+	unzip draco-node.zip
+	chmod 777 openfog
+	./openfog
